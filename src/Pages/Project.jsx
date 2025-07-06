dbd9d1477cffc6a94475ecff4c9a62a1e1ef835a
Shalini <122449558+shalini1008@users.noreply.github.com>
2025-07-06 23:40:29 +0530

    Update Project.jsx

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -178,7 +178,7 @@ export default function Project() {
           className="bg-pink-500 text-white text-center px-6 py-3 rounded-lg shadow-md hover:bg-pink-600 transition"
           onClick={() =>
             window.open(
-              "https://drive.google.com/file/d/1t-EW89ODI6MSSogLXlx2YisDX9rdDFRC/view?usp=sharing",
+              "https://drive.google.com/file/d/1v84-q3QKa_WCId9x-ECBLNBrvvtJMQyV/view?usp=sharing",
               "_blank"
             )
           }
